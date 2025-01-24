91d2a0ee1c8995a9449f6d8844707f7887e5b6ef
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-24 19:51:10 +0900

    FEAT: BoardScreen Route 추가

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
+import 'package:todo_list_application/core/routes/app_routes.dart';
 
 final class AppPages {
   const AppPages._();
 
-  static final String initialLocation = '/';
-  static final List<GetPage> pages = [];
+  static final String initialLocation = AppRoutes.boardScreen.name;
+  static final List<GetPage> pages = [
+    AppRoutes.boardScreen,
+  ];
 }
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,3 +1,11 @@
+import 'package:get/get.dart';
+import 'package:todo_list_application/app/domain/task_board/view/board_screen.dart';
+
 final class AppRoutes {
   const AppRoutes._();
+
+  static final GetPage boardScreen = GetPage(
+    name: '/',
+    page: () => BoardScreen(),
+  );
 }
