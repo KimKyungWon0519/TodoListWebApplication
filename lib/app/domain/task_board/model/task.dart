81e274f51cd7d5ad711915f6ae9f51648d539d17
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-29 00:10:52 +0900

    FEAT: Task 모델 생성

---
+++ lib/app/domain/task_board/model/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  final String title;
+  final String content;
+  final DateTime date;
+  final String assignee;
+
+  const Task({
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.assignee,
+  });
+}
