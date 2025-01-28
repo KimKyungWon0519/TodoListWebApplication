92b0fcf89b7d35c48774dc02f00758c44ca9cd6d
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-28 22:08:39 +0900

    FEAT: Task 카드 클릭 시 상세보기 다이얼로그 표시

--- lib/app/domain/task_board/view/widget/task_card.dart
+++ lib/app/domain/task_board/view/widget/task_card.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_list_application/app/domain/detail_task/view/detail_task_dialog.dart';
 
 class TaskCard extends StatelessWidget {
   const TaskCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8),
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          spacing: 8,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              spacing: 4,
-              children: [
-                Expanded(
-                  child: Text(
-                    '제목',
-                    style: TextStyle(fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () => Get.dialog(DetailTaskDialog()),
+      child: Card(
+        margin: EdgeInsets.all(8),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            spacing: 8,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                spacing: 4,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '제목',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Text('담당자'),
-              ],
-            ),
-            Divider(),
-            Text('2025.01.01'),
-          ],
+                  Text('담당자'),
+                ],
+              ),
+              Divider(),
+              Text('2025.01.01'),
+            ],
+          ),
         ),
       ),
     );
