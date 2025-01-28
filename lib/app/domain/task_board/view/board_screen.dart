aaa6838db74b2de5c6dc57dd56126d23489f3010
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-28 09:10:25 +0900

    FEAT: BoardScreen에 AppBar 추가

--- lib/app/domain/task_board/view/board_screen.dart
+++ lib/app/domain/task_board/view/board_screen.dart
@@ -5,6 +5,10 @@ class BoardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('투두'),
+      ),
+    );
   }
 }
