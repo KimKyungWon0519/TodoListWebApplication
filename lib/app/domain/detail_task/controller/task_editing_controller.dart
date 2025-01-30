cd5f43533c46094a755aacd54f106a5913b411a0
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-30 19:28:45 +0900

    FEAT: 편집 여부 플래그 생성

--- lib/app/domain/detail_task/controller/task_editing_controller.dart
+++ lib/app/domain/detail_task/controller/task_editing_controller.dart
@@ -4,6 +4,7 @@ import 'package:todo_list_application/app/domain/detail_task/model/task_type.dar
 class TaskEditingController {
   TaskType _taskType = TaskType.toDo;
   Task _task = Task.empty();
+  bool _isEditing = false;
 
   set taskType(TaskType newValue) => _taskType = newValue;
   TaskType get taskType => _taskType;
@@ -11,19 +12,26 @@ class TaskEditingController {
   set task(Task task) => _task = task;
   Task get task => _task;
 
-  set title(String title) => _task = _task.copyWith(title: title);
+  set title(String title) => _updateTask(_task.copyWith(title: title));
   String get title => _task.title;
 
-  set content(String content) => _task = _task.copyWith(content: content);
+  set content(String content) => _updateTask(_task.copyWith(content: content));
   String get content => _task.content;
 
-  set assignee(String assignee) => _task = _task.copyWith(assignee: assignee);
+  set assignee(String assignee) =>
+      _updateTask(_task.copyWith(assignee: assignee));
   String get assignee => _task.assignee;
 
-  set date(DateTime date) => _task = _task.copyWith(date: date);
+  set date(DateTime date) => _updateTask(_task.copyWith(date: date));
   DateTime get date => _task.date;
 
   bool isEmptyTask() {
     return _task.title.trim().isEmpty || _task.assignee.trim().isEmpty;
+  }
+
+  void _updateTask(Task task) {
+    _task = task;
+
+    _isEditing = true;
   }
 }
