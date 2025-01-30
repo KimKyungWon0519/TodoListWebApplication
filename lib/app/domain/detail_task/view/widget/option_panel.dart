46284a679510086a2f4f200c9e36c4bfe7250021
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-30 15:26:29 +0900

    FEAT: OptionPanel의 Dropdown 버튼에 Item 추가

--- lib/app/domain/detail_task/view/widget/option_panel.dart
+++ lib/app/domain/detail_task/view/widget/option_panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_application/app/domain/detail_task/model/task_type.dart';
 import 'package:todo_list_application/app/domain/detail_task/view/widget/base_field.dart';
 
 class OptionPanel extends StatelessWidget {
@@ -10,8 +11,11 @@ class OptionPanel extends StatelessWidget {
       spacing: 8,
       children: [
         DropdownMenu(
-          dropdownMenuEntries: [],
-          width: double.infinity,
+          dropdownMenuEntries: TaskType.values
+              .map(
+                (e) => DropdownMenuEntry(value: e, label: e.name),
+              )
+              .toList(),
         ),
         BaseField(labelText: '담당자'),
         BaseField(labelText: '날짜'),
