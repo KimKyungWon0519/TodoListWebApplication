d3e7fc28f46da4590b863590bc60aa7fd752ceab
KimKyungWon0519 <gwon080870@gmail.com>
2025-01-30 20:30:53 +0900

    FEAT: Init data 변경

--- lib/core/util/initial_data.dart
+++ lib/core/util/initial_data.dart
@@ -2,17 +2,60 @@ import 'package:todo_list_application/app/domain/task_board/model/task.dart';
 import 'package:todo_list_application/app/domain/task_board/model/task_type.dart';
 import 'package:todo_list_application/app/domain/task_board/model/tasks_with_type.dart';
 
-List<TasksWithType> initialData = List.generate(
-  TaskType.values.length,
-  (typeIndex) => TasksWithType(
-    type: TaskType.values[typeIndex],
-    tasks: List.generate(
-      TaskType.values.length - typeIndex,
-      (taskIndex) => Task(
-          title: TaskType.values[typeIndex].name * (taskIndex + 1),
-          content: '내용' * 10,
-          date: DateTime.now(),
-          assignee: '${TaskType.values[typeIndex].name} - $taskIndex'),
-    ),
+List<TasksWithType> initialData = [
+  TasksWithType(
+    type: TaskType.toDo,
+    tasks: [
+      Task(
+        title: '할 일 - 1',
+        content: '',
+        date: DateTime.now(),
+        assignee: '담당자',
+      ),
+      Task(
+        title: '운동하기',
+        content: '운동하기',
+        date: DateTime.now(),
+        assignee: '담당자.1',
+      ),
+      Task(
+        title: '밥먹기',
+        content: '밥먹기',
+        date: DateTime.now(),
+        assignee: '담당자.3',
+      ),
+    ],
   ),
-);
+  TasksWithType(
+    type: TaskType.urgent,
+    tasks: [
+      Task(
+        title: '공부하기',
+        content: '공부하기',
+        date: DateTime.now(),
+        assignee: '담당자.12',
+      ),
+      Task(
+        title: '책읽기',
+        content: '책읽기',
+        date: DateTime.now(),
+        assignee: '담당자.34',
+      ),
+    ],
+  ),
+  TasksWithType(
+    type: TaskType.inProgress,
+    tasks: [
+      Task(
+        title: '영화보기',
+        content: '영화보기',
+        date: DateTime.now(),
+        assignee: '담당자1',
+      ),
+    ],
+  ),
+  TasksWithType(
+    type: TaskType.completed,
+    tasks: [],
+  ),
+];
